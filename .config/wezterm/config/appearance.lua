6b1072129e22e37f98d4c4252b6d1a777ddc4e72
Till Fricke <ti.fricke@ostfalia.de>
2025-02-04 18:57:50 +0100

    change to updated wezterm config

---
+++ .config/wezterm/config/appearance.lua
@@ -0,0 +1,86 @@
+local gpu_adapters = require('utils.gpu-adapter')
+-- local backdrops = require('utils.backdrops')
+-- local colors = require('colors.custom')
+
+return {
+   max_fps = 120,
+   front_end = 'WebGpu',
+   webgpu_power_preference = 'HighPerformance',
+   webgpu_preferred_adapter = gpu_adapters:pick_best(),
+   -- webgpu_preferred_adapter = gpu_adapters:pick_manual('Dx12', 'IntegratedGpu'),
+   -- webgpu_preferred_adapter = gpu_adapters:pick_manual('Gl', 'Other'),
+   underline_thickness = '1.5pt',
+
+   -- cursor
+   animation_fps = 120,
+   cursor_blink_ease_in = 'Linear',
+   cursor_blink_ease_out = 'Linear',
+   default_cursor_style = 'BlinkingBar',
+   cursor_blink_rate = 900,
+
+   -- color scheme
+   -- colors = colors,
+   color_scheme = 'Catppuccin Mocha',
+
+   colors = {
+      tab_bar = {
+         background = 'rgba(30, 30, 46, 0.0)', --'#1e1e2e',
+      },
+   },
+   -- background
+   -- background = backdrops:initial_options(false), -- set to true if you want wezterm to start on focus mode
+   background = {
+      {
+         source = { Color = '#1e1e2e' }, -- Set your desired background color
+         height = '120%',
+         width = '120%',
+         vertical_offset = '-10%',
+         horizontal_offset = '-10%',
+         opacity = 0.9, -- Adjust transparency as needed
+      },
+   },
+
+   -- window_background_opacity = 0.1,
+   -- scrollbar
+   enable_scroll_bar = true,
+
+   -- tab bar
+   enable_tab_bar = true,
+   hide_tab_bar_if_only_one_tab = true,
+   use_fancy_tab_bar = false,
+   tab_max_width = 25,
+   show_tab_index_in_tab_bar = true,
+   show_new_tab_button_in_tab_bar = false,
+   switch_to_last_active_tab_when_closing_tab = true,
+
+   -- window
+   window_padding = {
+      left = 1,
+      right = 2,
+      top = 0,
+      bottom = 0,
+   },
+   adjust_window_size_when_changing_font_size = false,
+   window_close_confirmation = 'NeverPrompt',
+   -- window_frame = {
+   --    active_titlebar_bg = '#090909',
+   --    -- font = fonts.font,
+   --    -- font_size = fonts.font_size,
+   -- },
+   -- inactive_pane_hsb = {
+   --    saturation = 0.9,
+   --    brightness = 0.65,
+   -- },
+   inactive_pane_hsb = {
+      saturation = 1,
+      brightness = 1,
+   },
+
+   visual_bell = {
+      fade_in_function = 'EaseIn',
+      fade_in_duration_ms = 250,
+      fade_out_function = 'EaseOut',
+      fade_out_duration_ms = 250,
+      target = 'CursorColor',
+   },
+}
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,49 +1,19 @@
-local wezterm = require 'wezterm'
-local act = wezterm.action
+local Config = require('config')
 
-wezterm.on('format-window-title', function(tab)
-    local zoomed = tab.active_pane.is_zoomed and "[Z] " or ""
-    local username = os.getenv("USER") or "unknown"
-    local hostname = os.getenv("HOSTNAME") or "unknown"
-    local index = (#tab.tabs > 1) and string.format('%s@%s:', username, hostname) or ""
-    return zoomed .. index .. tab.active_pane.title
-end)
+-- require('utils.backdrops')
+--    -- :set_focus('#000000')
+--    -- :set_images_dir(require('wezterm').home_dir .. '/Pictures/Wallpapers/')
+--    :set_images()
+--    :random()
 
-wezterm.on('format-tab-title', function(tab)
-    local title = tab.active_pane.title
-    local symbol = title:match("nano") and "✏️" or "📁"
-    if tab.is_active then
-        return {{Background = {Color = '#7882d1'}}, {Text = ' ' .. symbol .. ' ' .. title .. ' '}}
-    end
-    return ' ' .. title .. ' '
-end)
+require('events.left-status').setup()
+require('events.right-status').setup({ date_format = '%a %H:%M:%S' })
+require('events.tab-title').setup({ hide_active_tab_unseen = false, unseen_icon = 'circle' })
+-- require('events.new-tab-button').setup()
 
-return {
-    enable_wayland = false,
-    mouse_bindings = {
-        {event = {Down = {button = 'Middle'}}, mods = 'NONE', action = act.PasteFrom("PrimarySelection")},
-        {event = {Down = {button = 'Right'}}, mods = 'NONE', action = wezterm.action_callback(function(window, pane)
-            if window:get_selection_text_for_pane(pane) ~= '' then
-                window:perform_action(act.CopyTo 'ClipboardAndPrimarySelection', pane)
-                window:perform_action(act.ClearSelection, pane)
-            else
-                window:perform_action(act.PasteFrom("PrimarySelection"), pane)
-            end
-        end)},
-    },
-    keys = {
-        {key = 'c', mods = 'CTRL|SHIFT', action = act.CopyTo("Clipboard")},
-        {key = 'v', mods = 'CTRL|SHIFT', action = act.PasteFrom("Clipboard")},
-        -- More keybindings...
-    },
-    font = wezterm.font_with_fallback({"JetBrainsMono Nerd Font Mono", "Courier 10 Pitch", "monospace"}),
-    font_size = 12,
-    color_scheme = "CustomScheme",
-    color_schemes = {
-        ["CustomScheme"] = {
-            foreground = "#E0E0E0",
-            background = "#19191f",
-            -- More color configs...
-        },
-    },
-}
+return Config:init()
+   :append(require('config.appearance'))
+   :append(require('config.bindings'))
+   :append(require('config.fonts'))
+   :append(require('config.general'))
+   :append(require('config.launch')).options
