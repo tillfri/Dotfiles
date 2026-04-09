5e7969feeeddeb1b778e91a39365a95b5b7ddcfb
tillfri <till-fricke@gmx.de>
2026-04-09 14:17:07 +0200

    fix treesitter

--- .config/nvim/lua/custom/plugins/treesitter.lua
+++ .config/nvim/lua/custom/plugins/treesitter.lua
@@ -1,75 +1,138 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-  },
-  build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  opts = {
-    ensure_installed = {
-      'lua',
-      'python',
-      'javascript',
-      'typescript',
-      'vimdoc',
-      'vim',
-      'regex',
-      'terraform',
-      'sql',
-      'dockerfile',
-      'toml',
-      'json',
-      'java',
-      'groovy',
-      'go',
-      'gitignore',
-      'graphql',
-      'yaml',
-      'make',
-      'cmake',
-      'markdown',
-      'markdown_inline',
-      'bash',
-      'tsx',
-      'css',
-      'html',
-      'hyprlang',
-      'cpp',
-      'latex',
-    }, -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ['aa'] = '@parameter.outer',
-          ['ia'] = '@parameter.inner',
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-          ['ac'] = '@class.outer',
-          ['ic'] = '@class.inner',
-          ['al'] = '@loop.outer',
-          ['il'] = '@loop.inner',
-          ['ii'] = '@conditional.inner',
-          ['ai'] = '@conditional.outer',
+  {
+    'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    lazy = false,
+    build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter').install {
+        'lua',
+        'python',
+        'javascript',
+        'typescript',
+        'vimdoc',
+        'vim',
+        'regex',
+        'terraform',
+        'sql',
+        'dockerfile',
+        'toml',
+        'json',
+        'java',
+        'groovy',
+        'go',
+        'gitignore',
+        'graphql',
+        'yaml',
+        'make',
+        'cmake',
+        'markdown',
+        'markdown_inline',
+        'bash',
+        'tsx',
+        'css',
+        'html',
+        'hyprlang',
+        'cpp',
+        'latex',
+      }
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = {
+          'lua',
+          'python',
+          'javascript',
+          'typescript',
+          'vimdoc',
+          'vim',
+          'regex',
+          'terraform',
+          'sql',
+          'dockerfile',
+          'toml',
+          'json',
+          'java',
+          'groovy',
+          'go',
+          'graphql',
+          'yaml',
+          'make',
+          'cmake',
+          'markdown',
+          'bash',
+          'tsx',
+          'css',
+          'html',
+          'cpp',
+          'latex',
+          'ruby',
         },
-      },
-    },
+        callback = function(ev)
+          vim.treesitter.start()
+          if ev.match ~= 'ruby' then
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+    end,
   },
-  -- There are additional nvim-treesitter modules that you can use to interact
-  -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    branch = 'main',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('nvim-treesitter-textobjects').setup {
+        select = { lookahead = true },
+        move = { set_jumps = true },
+      }
+
+      local sel = require 'nvim-treesitter-textobjects.select'
+      local mov = require 'nvim-treesitter-textobjects.move'
+
+      vim.keymap.set({ 'x', 'o' }, 'aa', function()
+        sel.select_textobject('@parameter.outer', 'textobjects')
+      end, { desc = 'Select outer parameter' })
+      vim.keymap.set({ 'x', 'o' }, 'ia', function()
+        sel.select_textobject('@parameter.inner', 'textobjects')
+      end, { desc = 'Select inner parameter' })
+      vim.keymap.set({ 'x', 'o' }, 'af', function()
+        sel.select_textobject('@function.outer', 'textobjects')
+      end, { desc = 'Select outer function' })
+      vim.keymap.set({ 'x', 'o' }, 'if', function()
+        sel.select_textobject('@function.inner', 'textobjects')
+      end, { desc = 'Select inner function' })
+      vim.keymap.set({ 'x', 'o' }, 'ac', function()
+        sel.select_textobject('@class.outer', 'textobjects')
+      end, { desc = 'Select outer class' })
+      vim.keymap.set({ 'x', 'o' }, 'ic', function()
+        sel.select_textobject('@class.inner', 'textobjects')
+      end, { desc = 'Select inner class' })
+      vim.keymap.set({ 'x', 'o' }, 'al', function()
+        sel.select_textobject('@loop.outer', 'textobjects')
+      end, { desc = 'Select outer loop' })
+      vim.keymap.set({ 'x', 'o' }, 'il', function()
+        sel.select_textobject('@loop.inner', 'textobjects')
+      end, { desc = 'Select inner loop' })
+      vim.keymap.set({ 'x', 'o' }, 'ai', function()
+        sel.select_textobject('@conditional.outer', 'textobjects')
+      end, { desc = 'Select outer conditional' })
+      vim.keymap.set({ 'x', 'o' }, 'ii', function()
+        sel.select_textobject('@conditional.inner', 'textobjects')
+      end, { desc = 'Select inner conditional' })
+
+      vim.keymap.set({ 'n', 'x', 'o' }, ']f', function()
+        mov.goto_next_start('@function.outer', 'textobjects')
+      end, { desc = 'Next function start' })
+      vim.keymap.set({ 'n', 'x', 'o' }, '[f', function()
+        mov.goto_previous_start('@function.outer', 'textobjects')
+      end, { desc = 'Previous function start' })
+      vim.keymap.set({ 'n', 'x', 'o' }, ']c', function()
+        mov.goto_next_start('@class.outer', 'textobjects')
+      end, { desc = 'Next class start' })
+      vim.keymap.set({ 'n', 'x', 'o' }, '[c', function()
+        mov.goto_previous_start('@class.outer', 'textobjects')
+      end, { desc = 'Previous class start' })
+    end,
+  },
 }
