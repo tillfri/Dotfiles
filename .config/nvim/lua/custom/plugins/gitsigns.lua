6094f238598d94b08d83e5c86b9709de318e932f
tillfri <till-fricke@gmx.de>
2026-04-25 00:56:16 +0200

    buff gitsigns

--- .config/nvim/lua/custom/plugins/gitsigns.lua
+++ .config/nvim/lua/custom/plugins/gitsigns.lua
@@ -16,5 +16,36 @@ return {
       topdelete = { text = '‾' },
       changedelete = { text = '~' },
     },
+    on_attach = function(bufnr)
+      local gitsigns = require 'gitsigns'
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- Navigation
+      map('n', ']c', function()
+        if vim.wo.diff then
+          vim.cmd.normal { ']c', bang = true }
+        else
+          gitsigns.nav_hunk 'next'
+        end
+      end)
+
+      map('n', '[c', function()
+        if vim.wo.diff then
+          vim.cmd.normal { '[c', bang = true }
+        else
+          gitsigns.nav_hunk 'prev'
+        end
+      end)
+
+      map('n', '<leader>fd', function()
+        gitsigns.diffthis '~1'
+      end)
+      map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+    end,
   },
 }
