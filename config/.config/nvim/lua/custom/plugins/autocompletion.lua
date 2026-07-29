814ca43048f3c2c1ff6b3c593b15274d330d3f89
tillfri <till-fricke@gmx.de>
2026-07-29 11:53:49 +0200

    test

--- config/.config/nvim/lua/custom/plugins/autocompletion.lua
+++ config/.config/nvim/lua/custom/plugins/autocompletion.lua
@@ -1,163 +1,120 @@
+-- blink links every BlinkCmpKind<Kind> to one group, so the menu renders in a
+-- single colour. Themes style nvim-cmp's CmpItem* groups instead, so map the
+-- kinds onto Treesitter groups, which every theme defines.
+local kind_hl = {
+  Text = '@string',
+  Method = '@function.method',
+  Function = '@function',
+  Constructor = '@constructor',
+  Field = '@property',
+  Property = '@property',
+  Variable = '@variable',
+  Reference = '@variable',
+  Class = '@type',
+  Interface = '@type',
+  Struct = '@type',
+  TypeParameter = '@type.parameter',
+  Module = '@module',
+  Unit = '@number',
+  Value = '@number',
+  Enum = '@constant',
+  EnumMember = '@constant',
+  Constant = '@constant',
+  Keyword = '@keyword',
+  Operator = '@operator',
+  Event = '@constant.macro',
+  Snippet = '@character.special',
+  Color = '@constant',
+  File = 'Directory',
+  Folder = 'Directory',
+}
+
+local function apply_highlights()
+  for kind, group in pairs(kind_hl) do
+    vim.api.nvim_set_hl(0, 'BlinkCmpKind' .. kind, { link = group, default = false })
+  end
+  -- Matched characters stand out, source column stays subdued.
+  vim.api.nvim_set_hl(0, 'BlinkCmpLabelMatch', { link = '@function', default = false })
+  vim.api.nvim_set_hl(0, 'BlinkCmpSource', { link = 'Comment', default = false })
+end
+
 return { -- Autocompletion
-  'hrsh7th/nvim-cmp',
+  'saghen/blink.cmp',
   dependencies = {
-    -- Snippet Engine & its associated nvim-cmp source
-    {
-      'L3MON4D3/LuaSnip',
-      build = (function()
-        -- Build Step is needed for regex support in snippets.
-        -- This step is not supported in many windows environments.
-        -- Remove the below condition to re-enable on windows.
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
-      dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        --    See the README about individual language/framework/plugin snippets:
-        --    https://github.com/rafamadriz/friendly-snippets
-        {
-          'rafamadriz/friendly-snippets',
-          config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
-          end,
-        },
+    'saghen/blink.lib',
+    'rafamadriz/friendly-snippets',
+  },
+  build = function()
+    require('blink.cmp').build():pwait()
+  end,
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = {
+      preset = 'none',
+      ['<Up>'] = { 'select_prev', 'fallback' },
+      ['<Down>'] = { 'select_next', 'fallback' },
+      ['<C-Space>'] = { 'accept', 'fallback' },
+      ['<C-l>'] = { 'snippet_forward', 'fallback' },
+      ['<C-h>'] = { 'snippet_backward', 'fallback' },
+      ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+      ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
+      ['<C-c>'] = { 'show', 'fallback' },
+    },
+    appearance = {
+      -- 'mono' for Nerd Font Mono, 'normal' for Nerd Font
+      nerd_font_variant = 'mono',
+      kind_icons = {
+        Text = '󰉿',
+        Method = '󰊕',
+        Function = '󰊕',
+        Constructor = '󰒓',
+        Field = '󰜢',
+        Variable = '󰆦',
+        Property = '󰖷',
+        Class = '󱡠',
+        Interface = '󱡠',
+        Struct = '󱡠',
+        Module = '󰅩',
+        Unit = '󰪚',
+        Value = '󰦨',
+        Enum = '󰦨',
+        EnumMember = '󰦨',
+        Keyword = '󰻾',
+        Constant = '󰏿',
+        Snippet = '󱄽',
+        Color = '󰏘',
+        File = '󰈔',
+        Reference = '󰬲',
+        Folder = '󰉋',
+        Event = '󱐋',
+        Operator = '󰪚',
+        TypeParameter = '󰬛',
       },
     },
-    'saadparwaiz1/cmp_luasnip',
-
-    -- Adds other completion capabilities.
-    --  nvim-cmp does not ship with all sources by default. They are split
-    --  into multiple repos for maintenance purposes.
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-  },
-  config = function()
-    -- See `:help cmp`
-    local cmp = require 'cmp'
-    local luasnip = require 'luasnip'
-    luasnip.config.setup {}
-
-    local kind_icons = {
-      Text = '󰉿',
-      Method = 'm',
-      Function = '󰊕',
-      Constructor = '',
-      Field = '',
-      Variable = '󰆧',
-      Class = '󰌗',
-      Interface = '',
-      Module = '',
-      Property = '',
-      Unit = '',
-      Value = '󰎠',
-      Enum = '',
-      Keyword = '󰌋',
-      Snippet = '',
-      Color = '󰏘',
-      File = '󰈙',
-      Reference = '',
-      Folder = '󰉋',
-      EnumMember = '',
-      Constant = '󰇽',
-      Struct = '',
-      Event = '',
-      Operator = '󰆕',
-      TypeParameter = '󰊄',
-    }
-    cmp.setup {
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      completion = { completeopt = 'menu,menuone,noinsert' },
-
-      -- For an understanding of why these mappings were
-      -- chosen, you will need to read `:help ins-completion`
-      --
-      -- No, but seriously. Please read `:help ins-completion`, it is really good!
-      mapping = cmp.mapping.preset.insert {
-        -- Select the [n]ext item
-        ['<Down>'] = cmp.mapping.select_next_item(),
-        -- Select the [p]revious item
-        ['<Up>'] = cmp.mapping.select_prev_item(),
-
-        -- Scroll the documentation window [b]ack / [f]orward
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-        -- Accept ([y]es) the completion.
-        --  This will auto-import if your LSP supports it.
-        --  This will expand snippets if the LSP sent a snippet.
-        ['<C-Space>'] = cmp.mapping.confirm { select = true },
-
-        -- If you prefer more traditional completion keymaps,
-        -- you can uncomment the following lines
-        --['<CR>'] = cmp.mapping.confirm { select = true },
-        --['<Tab>'] = cmp.mapping.select_next_item(),
-        --['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-        -- Manually trigger a completion from nvim-cmp.
-        --  Generally you don't need this, because nvim-cmp will display
-        --  completions whenever it has completion options available.
-        -- ['<C-CR>'] = cmp.mapping.complete {},
-
-        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-        -- Select next/previous item with Tab / Shift + Tab
-        ['<Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
-        -- ['<C-y>'] = cmp.mapping(function(fallback)
-        --   if cmp.visible() then
-        --     cmp.mapping.confirm { select = true }()
-        --   else
-        --     fallback()
-        --   end
-        -- end, { 'i', 's' }),
-      },
-      sources = {
-        {
-          name = 'lazydev',
-          -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-          group_index = 0,
+    completion = {
+      -- Equivalent of the old `completeopt=noinsert` + `confirm { select = true }`:
+      -- <CR> takes the first entry without selecting it first.
+      list = { selection = { preselect = true, auto_insert = false } },
+      menu = {
+        draw = {
+          columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 1 }, { 'source_name' } },
         },
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'buffer' },
-        { name = 'path' },
       },
-      formatting = {
-        fields = { 'kind', 'abbr', 'menu' },
-        format = function(entry, vim_item)
-          vim_item.kind = string.format('%s', kind_icons[vim_item.kind])
-          vim_item.menu = ({
-            nvim_lsp = '[LSP]',
-            luasnip = '[Snippet]',
-            buffer = '[Buffer]',
-            path = '[Path]',
-          })[entry.source.name]
-          return vim_item
-        end,
-      },
-    }
+      documentation = { auto_show = false },
+    },
+    sources = {
+      default = { 'lsp', 'snippets', 'buffer', 'path' },
+    },
+    fuzzy = { implementation = 'rust' },
+  },
+  config = function(_, opts)
+    require('blink.cmp').setup(opts)
+    apply_highlights()
+    -- Themes reset highlights when they load, so re-apply after a switch.
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      group = vim.api.nvim_create_augroup('blink-cmp-kind-colors', { clear = true }),
+      callback = apply_highlights,
+    })
   end,
 }
