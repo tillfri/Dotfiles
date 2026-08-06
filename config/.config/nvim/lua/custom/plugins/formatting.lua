3b5c26dacfd8710dd33bfc8de8e1a5f57f45377d
tillfri <till-fricke@gmx.de>
2026-08-06 13:51:44 +0200

    dont format .c files on save

--- config/.config/nvim/lua/custom/plugins/formatting.lua
+++ config/.config/nvim/lua/custom/plugins/formatting.lua
@@ -21,10 +21,15 @@ return {
     default_format_opts = {
       lsp_format = 'fallback',
     },
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_format = 'fallback',
-    },
+    format_on_save = function(bufnr)
+      if vim.bo[bufnr].filetype == 'c' then
+        return
+      end
+      return {
+        timeout_ms = 500,
+        lsp_format = 'fallback',
+      }
+    end,
     formatters = {
       shfmt = {
         append_args = { '-i', '4' },
